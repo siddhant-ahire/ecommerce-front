bcdf53f45fc6f22f9539fffbeb9a23b169bd04c9
siddhantahireofficial <siddhantahireofficial@gmail.com>
2021-05-11 20:27:35 +0530

    signin, signup, signout is done

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,33 +1,54 @@
 import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { Link, Redirect, withRouter } from 'react-router-dom'
+import { isAuthenticated, signout } from '../auth';
 
-const isActive = (history,path) =>{
-    if(history.location.pathname === path){
-        return {color:"#ff9900"};
+const isActive = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#ff9900" };
     } else {
-        return {color:"#ffffff"};
+        return { color: "#ffffff" };
     }
 }
 
-const Menu = ({history}) => {
+const Menu = ({ history }) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-primary">
                 <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history,'/')} to="/">
+                    <Link className="nav-link" style={isActive(history, '/')} to="/">
                         Home
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history,'/signin')} to="/signin">
-                        Signin
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history,'/signup')} to="/signup">
-                        Signup
-                    </Link>
-                </li>
+                {!isAuthenticated() && (
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
+                                Signin
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
+                                Signup
+                            </Link>
+                        </li>
+                    </>
+                )}
+                {isAuthenticated() && (
+                    <>
+                        <li className="nav-item">
+                            <span className="nav-link" style={{ cursor: 'pointer', color: '#ffffff' }}
+                                onClick={() => {
+                                    signout(() => {
+                                        history.push('/');
+                                    })
+                                }}>
+                                Signout
+                            </span>
+                        </li>
+                    </>
+                )
+
+                }
             </ul>
         </div>
     )
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,10 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout'
+import { authenticate, signin } from '../auth'
 
 const Signin = () => {
-    return (
-        <Layout title="Signin" description="Node React E-commerce App"></Layout>
 
+    const [values, setValues] = useState({
+        email: 'siddhantahire55@gmail.com',
+        password: 'Ahire@000',
+        error: '',
+        loading: false,
+        redirectToReferrer: false
+    });
+
+    const { email, password, error, loading, redirectToReferrer } = values;
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value })
+    };
+
+
+
+    const clickSubmit = (event) => {
+        event.preventDefault();
+        setValues({ ...values, error: false, loading:true })
+        signin({ email, password })
+            .then(data => {
+                if (data.error) {
+                    setValues({ ...values, error: data.error, loading: false })
+                } else {
+                    authenticate(
+                        data,
+                        () => {
+                            setValues({
+                                ...values,
+                                redirectToReferrer:true
+                            })
+                        })
+                }
+            })
+    }
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    )
+
+    const showLoading = () => (
+        loading && (
+            <div className="alert alert-info">
+                <h2>Loading...</h2>
+            </div>
+        )
+    )
+
+    const redirectUser = () => {
+        if(redirectToReferrer) {
+            return <Redirect to='/' />
+        }
+    }
+
+    const signInForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">email</label>
+                <input onChange={handleChange('email')} type="email" className="form-control" value={email} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control" value={password} />
+            </div>
+            <button onClick={clickSubmit} type='submit' className="btn btn-primary">Submit</button>
+        </form>
+    )
+
+    return (
+        <div>
+            <Layout title="Signin" description="Node React E-commerce App" className="container col-md-8 offset-md-2">
+                {showError()}
+                {showLoading()}
+                {signInForm()}
+                {redirectUser()}
+            </Layout>
+        </div>
     )
 }
 
